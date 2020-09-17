d327e0b7c409d62a401f5e260bcd220c10acc907
Olalekan Efunkunle <efunkunleolalekan@gmail.com>
2020-09-18 00:02:12 +0100

    few changes

--- src/Components/LayOut/Footer.js
+++ src/Components/LayOut/Footer.js
@@ -1,40 +1,22 @@
 import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import Navbar from 'react-bootstrap'
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import CopyRight from './CopyRight'
 
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '15vh',
-  },
-  main: {
-    marginTop: theme.spacing(8),
-    marginBottom: theme.spacing(2),
-  },
-  footer: {
-    padding: theme.spacing(3, 2),
-    marginTop: 'auto',
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
-  },
-}));
+
 
 export default function StickyFooter() {
-  const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <footer className={classes.footer}>
         <Container maxWidth="sm">
+        <Navbar sticky="bottom" >
           <Typography variant="body1">My sticky footer can be found here.</Typography>
           <CopyRight />
+          </Navbar>
         </Container>
-      </footer>
     </div>
   );
 }
