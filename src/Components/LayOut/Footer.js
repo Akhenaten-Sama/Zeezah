f9222c0d982ed6edf584736a84ede4d098f0b069
Olalekan Efunkunle <efunkunleolalekan@gmail.com>
2020-09-18 00:18:32 +0100

    added navbar footer

--- src/Components/LayOut/Footer.js
+++ src/Components/LayOut/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from 'react-bootstrap'
+import Navbar from 'react-bootstrap/Navbar'
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import CopyRight from './CopyRight'
